74cea9e8b9bc63eb9abfadd132ffe69ab0e168c5
3ismartyash <srisaiyaswanth333@gmail.com>
2024-10-27 01:28:52 +0530

    Adding Angular

---
+++ Angular/EmployeeProfile/src/app/app.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ListEmployeesComponent } from "./employees/employees/list-employees.component";
+
+
+
+
+
+@Component({
+
+
+
+ selector: 'app-root',
+
+
+
+ standalone: true,
+
+
+
+ imports: [RouterOutlet, ListEmployeesComponent], // Add ListEmployeesComponent here
+
+
+
+ templateUrl: './app.component.html',
+
+
+
+ styleUrls: ['./app.component.css']
+
+
+
+})
+
+
+
+export class AppComponent {
+
+
+
+ title = 'EmployeeProfile';
+
+
+
+}
+
+
+
+
+
+
+
